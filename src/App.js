89d7abb4dc7b539f233b025656448c5dfe6b6ccd
AlexeiMir <alex_16.07@mail.ru>
2021-08-18 19:14:47 +0700

    part 1. description effects

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {useDispatch, useSelector} from "react-redux";
+
+function App() {
+    const store = useSelector(store => store)
+    const dispatch = useDispatch()
+
+    console.log(store)
+
+  return (
+    <div className="App">
+      saga
+        <button onClick={() => dispatch({type:'LOAD_DATA'})}>click me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/sagas/index.js
@@ -0,0 +1,72 @@
+//  Выполняет бизнес логику  (запрос/ таумаут/ запись в кеш) .
+//  Запускается в зависимоти от какого-то выполненого экшена
+// тестят workerSaga
+// Effects - функции возвращающие простые объекты, которые содержат инструкции выполняющиеся самой sagaMiddleware
+// Effect прокидывает в redux-saga инструкции, которые ей надо выполнять
+// takeEvery создает и запускает worker сагу на каждый dispatch данного action
+// takeLatest - автоматически отменяет любую предыдущую задачу саги, запущенную ранее, если она все еще выполняется
+// takeLeading - Автоматически отменяет любую следующую задачу саги, запущенную позднее , если первая запущенная еше
+// выполняется
+// put (неблокирующий)- указывает middleware отправить действие в store. Вызывает dispatch c переданным action. Диспатчит изменения в store
+// call(блокирующий) - Выполняет вызов асинхронной функции. Если функция вернет promise, приостанавливает сагу до тех пор , пока
+// promise не вызовет resolve
+// take & call - блокирующие эффекты. takeEvery сам по себе не блокирующий, но внутри он использует take + fork
+// fork - эффект, который указывает middleware выполнить неблокирующий вызов переданной функции
+// (создает разветвленный процесс)
+// spawn - создает параллельную задачу в корне саги, сам процесс не привязан к родителю
+// join - заблокировать не блокирующую задачу и получить ее результат
+// select - получить данные из store, аналог useSelect/mapStateToProps
+// cancel - отменяет задачу
+// yield fork - создает объект с методами и свойствами, которые создает сага
+import {take, takeEvery, takeLatest, takeLeading,put,call, fork, spawn, join, select} from 'redux-saga/effects'
+// take(блокирующий) ждет какого либо диспатча экшена в нашем приложении и после того как он происходит, он может выполниться
+
+async function swapiGet(pattern) {
+    const request = await fetch(`http://swapi.dev/api/${pattern}`)
+    const data = await request.json()
+
+    return data
+}
+
+const wait = (t) => new Promise((resolve => {
+    setTimeout(resolve,t)
+}))
+
+function* loadPeople() {
+    const people = yield call(swapiGet, 'people')
+    yield put({type:'SET_PEOPLE', payload: people.results})
+    console.log('load people')
+    return people
+}
+function* loadPlanets() {
+    const planets = yield call(swapiGet, 'planets')
+    yield put({type:'SET_PLANETS', payload: planets.results})
+    console.log('load planets')
+}
+
+export function* workerSaga() {
+    console.log('run parallels tasks')
+    // const task = yield fork(loadPeople)
+    // yield spawn(loadPlanets)
+    // const people = yield join(task)
+    yield call(loadPeople)
+    yield call(loadPlanets)
+    const store = yield select(s => s)
+    console.log('finish parallels tasks', store)
+
+}
+
+//  Следит за dispatch'em экшена в приложении и запускает worker
+export function* watchLoadDataSaga() {
+    // while(true){
+    //     yield take('CLICK')
+    //
+    //     yield workerSaga()
+    // }
+    yield takeEvery('LOAD_DATA', workerSaga)
+}
+
+
+export default function* rootSaga() {
+    yield fork(watchLoadDataSaga)
+}
